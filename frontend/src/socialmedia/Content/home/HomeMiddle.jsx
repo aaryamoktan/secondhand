5b59d8a5b57dda1deafd547ec025934a002f2170
aarya moktan <aaryamoktan2055@gmail.com>
2024-08-10 11:13:08 -0700

    first commit

--- frontend/src/socialmedia/Content/home/HomeMiddle.jsx
+++ frontend/src/socialmedia/Content/home/HomeMiddle.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { SlOptionsVertical } from "react-icons/sl";
 const HomeMiddle = () => {
+    const [search,setsearch] = useState();
+    const mydate = new Date()
   return (
     <div style={{borderRight:"1px solid #d2cfcf"}}  className='lg:w-[60%] lg:h-[100vh] '>
         <div className=''>
-            <input style={{border:"2px solid #969696"}}  className='lg:w-[60%] lg:h-[5vh] rounded-lg ml-[20%] mt-10' type="text" placeholder='Search'/>
+            <input onChange={(e)=>{
+                setsearch(e.target.value)
+            }} style={{border:"2px solid #969696"}}  className='lg:w-[60%] lg:h-[5vh] rounded-lg ml-[20%] mt-10' type="text" placeholder='Search'/>
         </div>
+        <div className='lg:w-[70%] ml-[10%]  mt-10'><p className='flex justify-between'><p className='ml-20 font-serif text-[#494949]'>Profilename &nbsp; &nbsp; . {mydate.toLocaleTimeString()}</p><p className='text-[#494949]' ><SlOptionsVertical/></p></p></div>
     </div>
   )
 }
